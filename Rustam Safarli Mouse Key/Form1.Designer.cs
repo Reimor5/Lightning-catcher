1503bfaf57e1afe42002f5123d43d192816e494e
irtes <irtes@LAPTOP-P6GILUQF>
2022-11-21 08:52:42 +0200

    Add project files.

---
+++ Rustam Safarli Mouse Key/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Rustam_Safarli_Mouse_Key
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Rustam_Safarli_Mouse_Key.Properties.Resources.Bolti;
+            this.pictureBox2.Location = new System.Drawing.Point(617, 80);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(124, 95);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Rustam_Safarli_Mouse_Key.Properties.Resources.Bolt;
+            this.pictureBox1.Location = new System.Drawing.Point(64, 261);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(119, 123);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "SCORE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Rustam Safarli Mouse Key/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rustam_Safarli_Mouse_Key
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int x, y, x2, y2, r, s, score=0;
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox2.Bounds.IntersectsWith(pictureBox1.Bounds))
+            {
+               
+                timer1.Stop();
+                MessageBox.Show("ZAP!!! Lightning caught!");
+                pictureBox1.Location = new Point(64, 261);
+                pictureBox2.Location = new Point(617, 80);
+                score++;
+                label2.Text = score.ToString();
+                timer1.Start();
+                pictureBox1.Location = new Point(64, 261);
+                pictureBox2.Location = new Point(617, 80);
+
+            }
+            if (r < x2)
+            {
+                pictureBox2.Location = new Point(r, s);
+                r+=10;
+            }
+            else
+            {
+                pictureBox2.Location = new Point(r, s);
+                r-=10;
+            }
+            if (s < y2)
+            {
+                pictureBox2.Location = new Point(r, s);
+                s+=10;
+            }
+            else
+            {
+                pictureBox2.Location = new Point(r, s);
+                s-=10;
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            x2 = e.X;
+            y2 = e.Y;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+          
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            x = pictureBox1.Location.X;
+            y = pictureBox1.Location.Y;
+            if (e.KeyCode == Keys.Left && x > 0)
+            {
+                x -= 5;
+            }
+            if (e.KeyCode == Keys.Right && x < 816)
+            {
+                x += 5;
+            }
+            if (e.KeyCode == Keys.Up && y > 0)
+            {
+                y -= 5;
+            }
+            if (e.KeyCode == Keys.Down && y < 489)
+            {
+                y += 5;
+            }
+            pictureBox1.Location = new Point(x, y);
+        }
+    }
+}
